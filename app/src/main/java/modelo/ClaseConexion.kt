60ea1d5526fbd4852a790befd38f47a478d8b03a
Rodrigo <20230333@ricaldone.edu.sv>
2024-06-05 16:52:43 -0600

    ya se pueden agregar datos

--- app/src/main/java/modelo/ClaseConexion.kt
+++ app/src/main/java/modelo/ClaseConexion.kt
@@ -6,7 +6,7 @@ import java.sql.DriverManager
 class ClaseConexion {
     fun cadenaConexion(): Connection?{
         try {
-            val url = "jdbc:oracle:thin:@10.10.0.79:1521:xe" // cambiar ip si se usa en otra pc
+            val url = "jdbc:oracle:thin:@192.168.1.2:1521:xe" // cambiar ip si se usa en otra pc
             val usuario = "system" //cambiar si se usa otro usuario
             val contrasena = "desarrollo" //cambiar si se usa otro usuario
 
--- app/src/main/java/rodrigo/cordova/aplicacioncrud/MainActivity.kt
+++ app/src/main/java/rodrigo/cordova/aplicacioncrud/MainActivity.kt
@@ -7,6 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import modelo.ClaseConexion
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,8 +29,28 @@ class MainActivity : AppCompatActivity() {
         val txtDescripcion = findViewById<TextView>(R.id.txtDescripcion)
         val txtFechaCreacion = findViewById<TextView>(R.id.txtFechaCreacion)
         val txtEstado = findViewById<TextView>(R.id.txtEstado)
+        val txtFechaFinalizacion = findViewById<TextView>(R.id.txtFechaFinalizacion)
+        val txtUsuario = findViewById<TextView>(R.id.txtUsuario)
         val btnAgregar = findViewById<Button>(R.id.btnAgregar)
 
+        //programo boton
+        btnAgregar.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                //crear un obj conexion
+                val objConexion = ClaseConexion().cadenaConexion()
 
+                //preparestatement
+                val addTicket = objConexion?.prepareStatement("insert into tbTicket(NumeroTicket, tituloTicket, descripcion, fechaCreacion, estadoTicket, fechaFinalizacion, usuario) values(?, ?, ?, ?, ?, ?, ?)")!!
+
+                addTicket.setString(1, UUID.randomUUID().toString())
+                addTicket.setString(2, txtTitulo.text.toString())
+                addTicket.setString(3, txtDescripcion.text.toString())
+                addTicket.setString(4, txtFechaCreacion.text.toString())
+                addTicket.setString(5, txtEstado.text.toString())
+                addTicket.setString(6, txtFechaFinalizacion.text.toString())
+                addTicket.setString(7, txtUsuario.text.toString())
+                addTicket.executeUpdate()
+            }
+        }
     }
 }
