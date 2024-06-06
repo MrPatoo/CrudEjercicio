91122ce7ab5eb795a853589f51ae8889261e3522
Rodrigo <20230333@ricaldone.edu.sv>
2024-06-05 20:22:20 -0600

    ya sale toda la info en los cards

--- app/src/main/java/RecyclerViewHelpers/Adaptador.kt
+++ app/src/main/java/RecyclerViewHelpers/Adaptador.kt
@@ -18,9 +18,14 @@ class Adaptador(var Datos: List<tbTicket>):RecyclerView.Adapter<ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-
+        //contenido de la card
         val item = Datos[position]
         holder.txtNombreCard.text = item.tituloTicket
+        holder.txtDescripcionCard.text = item.descripcion
+        holder.txtEstadoCard.text = item.estadoTicket
+        holder.txtFechaCreacionCard.text = item.fechaCreacion
+        holder.txtFechaFinalizacionCard.text = item.fechaFinalizacion
+        holder.txtUsuarioCard.text = item.usuario
     }
 
 }
--- app/src/main/java/RecyclerViewHelpers/ViewHolder.kt
+++ app/src/main/java/RecyclerViewHelpers/ViewHolder.kt
@@ -7,6 +7,12 @@ import rodrigo.cordova.aplicacioncrud.R
 
 class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
+    //contenido de la card
     val txtNombreCard = view.findViewById<TextView>(R.id.txtTituloCard)
+    val txtDescripcionCard = view.findViewById<TextView>(R.id.txtDescripcionCard)
+    val txtEstadoCard = view.findViewById<TextView>(R.id.txtEstadoCard)
+    val txtFechaCreacionCard = view.findViewById<TextView>(R.id.txtFechaCreacionCard)
+    val txtFechaFinalizacionCard = view.findViewById<TextView>(R.id.txtFechaFinalizacionCard)
+    val txtUsuarioCard = view.findViewById<TextView>(R.id.txtUsuarioCard)
 
 }
